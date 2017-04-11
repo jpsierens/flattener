6017a8274786ab0e11d881777bbb071450eefe49
jpsierens <jsierens@tacitknowledge.com>
2017-04-11 17:35:42 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+
+const path = process.argv.slice(2);
+
+const flattener = (rootDir, cb) => {
+    console.log(`you chose to flatten ${path}`);
+    console.log('But I really havent implemented anything yet :p');
+};
+
+flattener(path);
